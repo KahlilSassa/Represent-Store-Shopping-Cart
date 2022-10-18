e73488d00e29f6d7280e4bf830cc09ee20da36d9
Kahlil Sassa <kahlilsassa@gmail.co>
2022-10-18 18:42:28 -0500

    added in carosel images for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,46 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.shopify.com/s/files/1/0587/5816/8785/files/web_banner_247_winter__0001_DSC00380_2048x.jpg?v=1665938094"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>24 Explore</h3>
+          <p>Shop Now</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.shopify.com/s/files/1/0587/5816/8785/files/blanks_fw22_banners_0001_Layer_3_2048x.jpg?v=1664984501"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Winter Basics</h3>
+          <p>Shop Now</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.shopify.com/s/files/1/0587/5816/8785/files/d2DESK_2048x.jpg?v=1664381685"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>FW22 COLLECTION</h3>
+          <p>
+            Shop Now
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
