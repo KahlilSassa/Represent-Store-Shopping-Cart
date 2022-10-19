e08bf3386436edaaa0cfc10adfa2371a9a6fdbca
Kahlil Sassa <kahlilsassa@gmail.co>
2022-10-18 22:19:37 -0500

    removed deps and about pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
-import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
